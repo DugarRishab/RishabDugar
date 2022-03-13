0773640a2fabda4afa1ece330be6535a61379d83
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-03-13 19:10:25 +0530

    Created the Home page

--- style.js
+++ style.js
@@ -12,24 +12,41 @@ window.onload = () => {
 	const mouseBgCircle = document.querySelector(".bg-circle");
 	const mouseHoverText = document.querySelectorAll(".mouseHover");
 
-	mouseHoverText.forEach(item => {
-		item.addEventListener("mouseover", () => {
+	// mouseHoverText.forEach(item => {
+	// 	item.addEventListener("mouseover", () => {
+	// 		mouseBgCircle.style.opacity = "0%";
+	// 		mouseCircle.style.transform = "scale(2)";
+	// 	});
+	// 	item.addEventListener("mousemouve", () => {
+	// 		mouseBgCircle.style.opacity = "0%";
+	// 		mouseCircle.style.transform = "scale(2)";
+	// 	});
+
+	// 	item.addEventListener("mouseout", e => {
+	// 		e.preventDefault();
+
+	// 		mouseBgCircle.style.opacity = "100%";
+	// 		mouseCircle.style.transform = "scale(1)";
+	// 	});
+
+	// });
+
+	document.addEventListener('mousemove', e => {
+		const tgt = e.target;
+		const inline = tgt.style.cursor || "Not defined"
+		const computed = window.getComputedStyle(tgt)["cursor"]
+		//console.log("Inline: ",inline,"Computed: ",computed)
+		
+		if (computed === 'pointer') {
 			mouseBgCircle.style.opacity = "0%";
 			mouseCircle.style.transform = "scale(2)";
-		});
-		item.addEventListener("mousemouve", () => {
-			mouseBgCircle.style.opacity = "0%";
-			mouseCircle.style.transform = "scale(2)";
-		});
-
-		item.addEventListener("mouseout", e => {
-			e.preventDefault();
-
+		}
+		else {
 			mouseBgCircle.style.opacity = "100%";
 			mouseCircle.style.transform = "scale(1)";
-		});
-
+		}
 	});
+	
 
 	document.addEventListener("click", () => {
 		//mouseCircle.style.opacity = "0%";
